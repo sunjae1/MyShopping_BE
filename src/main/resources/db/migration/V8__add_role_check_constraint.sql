f729cf2daeb7e1f2c2c0dcd1a3c50e91f18af63e
sunjae1 <mk35plkr@naver.com>
2026-03-06 05:25:27 +0900

    [fix]DB Migration

--- src/main/resources/db/migration/V8__add_role_check_constraint.sql
+++ src/main/resources/db/migration/V8__add_role_check_constraint.sql
@@ -1,2 +1,2 @@
 -- MEMBER 테이블의 role 컬럼에 'USER', 'ADMIN'만 들어올 수 있도록 체크 제약 조건 추가
-ALTER TABLE MEMBER ADD CONSTRAINT chk_role CHECK (role IN ('USER', 'ADMIN'));
+ALTER TABLE member ADD CONSTRAINT chk_role CHECK (role IN ('USER', 'ADMIN'));
