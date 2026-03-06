f3d5d33902185d501a5368cbf5f2864b7d8c03b3
sunjae1 <mk35plkr@naver.com>
2026-03-06 17:51:34 +0900

    [fix] User Authentication in Comment and Post Controllers

--- src/main/java/myex/shopping/controller/web/CommentController.java
+++ src/main/java/myex/shopping/controller/web/CommentController.java
@@ -1,43 +1,40 @@
 package myex.shopping.controller.web;
 
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import myex.shopping.domain.Comment;
-import myex.shopping.domain.Post;
 import myex.shopping.domain.User;
 import myex.shopping.dto.postdto.PostDBDto;
+import myex.shopping.dto.userdto.PrincipalDetails;
 import myex.shopping.dto.userdto.UserDto;
-import myex.shopping.exception.ResourceNotFoundException;
 import myex.shopping.form.CommentForm;
-import myex.shopping.repository.CommentRepository;
-import myex.shopping.repository.PostRepository;
 import myex.shopping.service.CommentService;
 import myex.shopping.service.PostService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 @RequestMapping("/posts")
 @RequiredArgsConstructor
 public class CommentController {
-    private final PostRepository postRepository;
-    private final CommentRepository commentRepository;
     private final CommentService commentService;
     private final PostService postService;
 
     // 댓글 추가
     @PostMapping("/{postId}/comments")
     public String addComment(@PathVariable Long postId,
-            @Valid @ModelAttribute CommentForm form,
-            BindingResult bindingResult,
-            HttpSession session,
-            Model model) {
-        User loginUser = (User) session.getAttribute("loginUser");
+                             @Valid @ModelAttribute CommentForm form,
+                             BindingResult bindingResult,
+                             @AuthenticationPrincipal PrincipalDetails principalDetails,
+                             Model model) {
+        User loginUser = principalDetails != null ? principalDetails.getUser() : null;
         if (loginUser != null) {
             UserDto userDto = new UserDto(loginUser);
             model.addAttribute("user", userDto);
@@ -59,15 +56,15 @@ public class CommentController {
     // 댓글 수정
     @PostMapping("/{postId}/comments/{commentId}/update")
     public String updateComment(@PathVariable Long postId,
-            @PathVariable Long commentId,
-            @Valid @ModelAttribute("commentToUpdate") CommentForm form,
-            BindingResult bindingResult,
-            HttpSession session,
-            Model model) {
+                                @PathVariable Long commentId,
+                                @Valid @ModelAttribute("commentToUpdate") CommentForm form,
+                                BindingResult bindingResult,
+                                @AuthenticationPrincipal PrincipalDetails principalDetails,
+                                Model model) {
         // 폼 바인딩 확인
         log.info("CommentForm 정보: {}", form);
         // 로그인 확인
-        User loginUser = (User) session.getAttribute("loginUser");
+        User loginUser = principalDetails != null ? principalDetails.getUser() : null;
         if (loginUser != null) {
             UserDto userDto = new UserDto(loginUser);
             model.addAttribute("user", userDto);
@@ -85,7 +82,7 @@ public class CommentController {
             return "/posts/view";
         }
         // 작성자 본인만 수정 가능.
-        if (commentService.isCommentOwner(commentId, loginUser)) {
+        if (loginUser != null && commentService.isCommentOwner(commentId, loginUser)) {
             commentService.updateComment(commentId, form, loginUser.getId());
             log.info("댓글 수정 메소드 후 commentService.updateComment 후");
         }
@@ -95,10 +92,9 @@ public class CommentController {
     // 댓글 삭제
     @PostMapping("/{postId}/comments/{commentId}")
     public String deleteComment(@PathVariable Long postId,
-            @PathVariable Long commentId,
-            HttpSession session,
-            Model model) {
-        User loginUser = (User) session.getAttribute("loginUser");
+                                @PathVariable Long commentId,
+                                @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        User loginUser = principalDetails != null ? principalDetails.getUser() : null;
         // 작성자 본인만 삭제 가능
         if (commentService.isCommentOwner(commentId, loginUser)) {
             commentService.deleteComment(postId, commentId);
--- src/main/java/myex/shopping/controller/web/PostController.java
+++ src/main/java/myex/shopping/controller/web/PostController.java
@@ -1,23 +1,22 @@
 package myex.shopping.controller.web;
 
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import myex.shopping.domain.User;
 import myex.shopping.dto.postdto.PostDBDto;
 import myex.shopping.dto.postdto.PostEditDto;
+import myex.shopping.dto.userdto.PrincipalDetails;
 import myex.shopping.dto.userdto.UserDto;
-import myex.shopping.exception.ResourceNotFoundException;
 import myex.shopping.form.CommentForm;
 import myex.shopping.form.PostForm;
 import myex.shopping.repository.PostRepository;
 import myex.shopping.service.PostService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -25,11 +24,11 @@ import java.util.List;
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/posts")
-// @Validated //--> BindingResult 전에 예외 터트려서 전역으로 처리하게 함.(Valid + BindingResult
-// 사용 못함.)
-// 거의 클래스 레벨에서 사용해야 함. 메소드 위에 선언시 객체만 검증 가능.(ModelAttribute, RequestBody)
-// 메서드 위는 단일 값 검증 RequestParam, PathVariable은 검증 못함. --> 단일 값 검증은 컨트롤러 안에서
-// if문으로.
+// @Validated -> BindingResult 전에 예외 터트려서 전역으로 처리
+// (Valid + BindingResult 이전 처리)
+// 클래스 레벨 사용.
+// 메소드 레벨 선언시 객체만 검증 가능.(ModelAttribute, RequestBody)
+// 메서드 레벨 선언시 단일 값 검증 RequestParam, PathVariable 검증 불가.
 public class PostController {
     private final PostRepository postRepository;
     private final PostService postService;
@@ -37,15 +36,14 @@ public class PostController {
     // 게시판 조회.
     @GetMapping
     public String list(Model model,
-            HttpSession session) {
-        User loginUser = (User) session.getAttribute("loginUser");
+                       @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        User loginUser = principalDetails != null ? principalDetails.getUser() : null;
         if (loginUser != null) {
             UserDto userDto = new UserDto(loginUser);
             model.addAttribute("user", userDto);
             model.addAttribute("loginUser", userDto);
         }
         List<PostDBDto> posts = postService.findAllPostDBDto();
-        // User loginUser = (User) session.getAttribute("loginUser");
         model.addAttribute("posts", posts);
         return "posts/list";
     }
@@ -53,26 +51,16 @@ public class PostController {
     // 게시물 한개 상세 보기.
     @GetMapping("/{id}")
     public String view(@PathVariable Long id,
-            Model model,
-            HttpSession session,
-            RedirectAttributes redirectAttributes) {
+                       Model model,
+                       @AuthenticationPrincipal PrincipalDetails principalDetails) {
         log.info("post.id = {}", id);
-        User loginUser = (User) session.getAttribute("loginUser");
+        User loginUser = principalDetails != null ? principalDetails.getUser() : null;
         if (loginUser != null) {
             UserDto userDto = new UserDto(loginUser);
             model.addAttribute("loginUser", userDto);
             model.addAttribute("user", userDto);
         }
-        /*
-         * if (postOpt.isEmpty()) {
-         * redirectAttributes.addFlashAttribute("errorPV","유효하지 않은 게시물입니다.");
-         * return "redirect:/posts";
-         * }
-         */
-        // LazyInitializationException 방지 위해서 DTO로 전환.
-        // 뷰에서 LAZY 필드 접근 시 예외 막기 위해.
         PostDBDto postDBDto = postService.changeToDto(id);
-        // model.addAttribute("loginUser",loginUser);
         model.addAttribute("post", postDBDto);
         model.addAttribute("commentForm", new CommentForm());
         return "posts/view";
@@ -81,8 +69,8 @@ public class PostController {
     // 게시물 등록 폼
     @GetMapping("/new")
     public String createForm(Model model,
-            HttpSession session) {
-        User loginUser = (User) session.getAttribute("loginUser");
+                             @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        User loginUser = principalDetails != null ? principalDetails.getUser() : null;
         if (loginUser != null) {
             UserDto userDto = new UserDto(loginUser);
             model.addAttribute("user", userDto);
@@ -92,21 +80,19 @@ public class PostController {
     }
 
     // 게시물 등록
-    // <!--post(Form) : title, content, userId-->
-    // Post(domain) : id(DB), title, content, userId, author, createdDate, comments
     @PostMapping("/new")
     public String create(@Valid @ModelAttribute("post") PostForm form,
-            BindingResult bindingResult,
-            Model model,
-            HttpSession session) {
-        User loginUser = (User) session.getAttribute("loginUser");
-        if (loginUser != null) {
-            UserDto userDto = new UserDto(loginUser);
-            model.addAttribute("user", userDto);
+                         BindingResult bindingResult,
+                         Model model,
+                         @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        if (principalDetails == null) {
+            return "redirect:/login";
         }
+        User loginUser = principalDetails.getUser();
+        UserDto userDto = new UserDto(loginUser);
+        model.addAttribute("user", userDto);
         if (bindingResult.hasErrors()) {
             log.info("게시물 등록 폼 검증 실패 : {}", bindingResult);
-            // model.addAttribute("user", loginUser);
             return "posts/new";
         }
         postService.createPost(form, loginUser);
@@ -116,10 +102,9 @@ public class PostController {
     // 게시물 수정 폼
     @GetMapping("/{id}/update")
     public String updateForm(@PathVariable Long id,
-            Model model,
-            HttpSession session) {
-
-        User loginUser = (User) session.getAttribute("loginUser");
+                             Model model,
+                             @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        User loginUser = principalDetails != null ? principalDetails.getUser() : null;
         if (loginUser != null) {
             UserDto userDto = new UserDto(loginUser);
             model.addAttribute("user", userDto);
@@ -132,13 +117,13 @@ public class PostController {
     // 게시물 수정
     @PostMapping("/{id}/update")
     public String updatePost(@PathVariable Long id,
-            @Valid @ModelAttribute("post") PostEditDto form,
-            BindingResult bindingResult,
-            HttpSession session,
-            Model model) {
+                             @Valid @ModelAttribute("post") PostEditDto form,
+                             BindingResult bindingResult,
+                             @AuthenticationPrincipal PrincipalDetails principalDetails,
+                             Model model) {
         log.info("게시물 수정 요청 컨트롤러 진입");
         log.info("PostForm 정보 : {}", form);
-        User loginUser = (User) session.getAttribute("loginUser");
+        User loginUser = principalDetails != null ? principalDetails.getUser() : null;
         if (loginUser != null) {
             UserDto userDto = new UserDto(loginUser);
             model.addAttribute("user", userDto);
@@ -149,16 +134,13 @@ public class PostController {
         }
         postService.updatePost(id, form);
         return "redirect:/posts/{id}";
-        // @PostMapping("/{postId}/update") 경로 변수 {postId} 는 redirect:/{postId} 플레이스홀더
-        // 값이랑 매칭 됨.
+        // @PostMapping("/{postId}/update") 경로 변수 {postId} 는 redirect:/{postId} 플레이스홀더 값이랑 매칭 됨.
     }
 
     // 게시물 삭제.
     @PostMapping("/{id}/delete")
     public String delete(@PathVariable Long id,
-            @RequestParam(required = false) String redirectInfo,
-            RedirectAttributes redirectAttributes) {
-        // 항상 null 가능성이 있는 변수를 .equals() 앞에 쓰면 NPE 위험
+                         @RequestParam(required = false) String redirectInfo) {
         log.info("redirectInfo 값 : {}", redirectInfo);
         postRepository.deleteById(id);
         if ("mypage".equals(redirectInfo)) {
